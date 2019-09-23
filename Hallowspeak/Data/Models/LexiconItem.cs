b9f439bdc8709fdc4939883f9d8dfba07958c840
dylan@fampiera.nl <dylan@fampiera.nl>
2019-09-23 14:29:51 +0200

    Load Lexicon from Database

---
+++ Hallowspeak/Data/Models/LexiconItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hallowspeak.Data.Models
+{
+    public class LexiconItem
+    {
+        public int Id { get; private set; }
+        public Dictionary<string, string> KeyValues = new Dictionary<string, string>();
+
+        public LexiconItem(int id)
+        {
+            Id = id;
+        }
+    }
+}
